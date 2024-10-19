fa5d42626299dccd9ee95213801bafa3fb66bfd3
Sarwar Jahan <sarwar3@gmail.com>
2024-10-19 11:12:20 +0600

    final update

--- student_result/result.php
+++ student_result/result.php
@@ -2,19 +2,20 @@
 
 // Taking students 5 subjects marks as default in parameters;
 function calculateResult(
-    $bangla = 59, //default value of Bangla
-    $english = 35,//default value of English
-    $physics = 60,//default value of Physics
-    $maths = 80, //default value of Mathematics
-    $chemistry = 95,//default value of Chemistry
+    $bangla = 59, //sample default value of Bangla
+    $english = 35,//sample default value of English
+    $physics = 60,//sample default value of Physics
+    $maths = 80, //sample default value of Mathematics
+    $chemistry = 95,//sample default value of Chemistry
 ) {
 
     $total_marks = $bangla + $english + $physics + $maths + $chemistry;
     $avg_marks = $total_marks / 5;
 
     // Grading the students marks using switch-Case starts here
-    $countGrade = "";//taking a variable to receive the value
-    switch ($avg_marks) {
+
+    $countGrade = ""; //taking a variable to receive the value
+    switch (true) {
         case ($avg_marks >= 80 && $avg_marks <= 100): {
             $countGrade = "A+";
             break;
@@ -49,6 +50,7 @@ function calculateResult(
     // Grading the students marks using switch-Case ends here
 
     $student_grades = "Total marks: {$total_marks}<br> Average marks: {$avg_marks}<br> Grade: {$countGrade}";
+
     //marks range validation starts here (using if-else) 
     if (
         ($bangla < 0 or $bangla > 100) or ($english < 0 or $english > 100)
@@ -56,26 +58,25 @@ function calculateResult(
         or ($chemistry < 0 or $chemistry > 100)
     ) {
         echo "mark range is invalid";
+    } elseif (
+        $avg_marks < 33 or
+        ($bangla < 33) or ($english < 33)
+        or ($physics < 33) or ($maths < 33)
+        or ($chemistry < 33)
+    ) {
+        echo "You have failed the test";
+
     } else {
-        if (
-            $avg_marks < 33 or
-            ($bangla < 33) or ($english < 33)
-            or ($physics < 33) or ($maths < 33)
-            or ($chemistry < 33)
-        ) {
-            echo "You have failed the test";
-
-        } else {
-            echo $student_grades;
-        }
+        echo $student_grades;
     }
-
-    //marks range validation ends here (using if-else)
-
 }
 
+//marks range validation ends here (using if-else)
+
+
+
 //lets call the function for Result Output
-calculateResult(83, 36, 45, 40, 45);
+calculateResult(35, 80, 40, 40, 45);
 
 //Goodbye, you reached at the end of the program
 
